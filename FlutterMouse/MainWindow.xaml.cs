fe8ec58e5712f0b3a94a252e154c5dc8c1643a6f
tinnuadan <tinnuadan@gmail.com>
2019-03-17 13:49:40 +0100

    MainWindow Gui & Code stub

--- FlutterMouse/MainWindow.xaml.cs
+++ FlutterMouse/MainWindow.xaml.cs
@@ -12,17 +12,64 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WindowsInput.Native;
+using WindowsInput;
+using System.Windows.Threading;
+using System.Timers;
 
 namespace FlutterMouse
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    #region Private Structs
+    private class LabelProps
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+      public Brush FG { get; set; }
+      public Brush BG { get; set; }
+
+      public LabelProps(Brush fg, Brush bg)
+      {
+        FG = fg;
+        BG = bg;
+      }
     }
+
+    private enum Speed
+    {
+      Fast, Medium, Slow
+    }
+    #endregion
+
+    #region Members
+    private HotKey hotKey { get; set; }
+    private InputSimulator inputSimulator { get; set; }
+    private Timer timer { get; set; }
+    private DispatcherTimer uiTimer { get; set; }
+    private List<LabelProps> lblActiveProps { get; set; }
+    //private List<AfkAction> actions { get; set; }
+
+    private int run;
+    //private AfkAction _currentAction;
+    #endregion
+
+    #region Public Methods
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    #endregion
+
+    #region Private Methods
+    private void cbMode_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      //var action = getSelectedAction();
+      //cbSpeed.IsEnabled = (action.Type == AfkAction.ActionType.Repeated);
+    }
+    #endregion
+  }
 }
